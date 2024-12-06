a70d96f0dacef924c6c7a6d68c5b584550984156
Gabrielle <gabrielle97oliveira@gmail.com>
2024-12-06 18:00:12 +0000

    Initial Commit

---
+++ BlissChallengeTest/BlissChallengeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlissChallengeTestApp.swift
+//  BlissChallengeTest
+//
+//  Created by Gabrielle Oliveira on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BlissChallengeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
